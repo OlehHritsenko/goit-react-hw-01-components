547bf5aa04b8ea5b803f5e1672951ff38941aa3f
OlehHritsenko <grolan12345@gmail.com>
2022-10-30 17:23:39 +0200

    create components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import css from 'App.module.css';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+import { Profile } from './components/Profile/Profile';
+import { Statistics } from './components/Statistics/Statistics';
+import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from 'components/Transaction/TransactionHistory';
+
+export const App = () => {
+  return (
+    <div className={css.App}>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers.toLocaleString('ru')}
+        views={user.stats.views.toLocaleString('ru')}
+        likes={user.stats.likes.toLocaleString('ru')}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
